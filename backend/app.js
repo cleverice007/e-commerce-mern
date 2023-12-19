b976cc799107ad4414a2ef031934f2d4dbce0642
cleverice007 <cleverice90128@gmail.com>
2023-12-19 20:20:55 +0800

    app.js 引入db config and connect

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,10 @@
 import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
+import connectDB from './config/db.js';
 const port = process.env.PORT || 5000;
+
+connectDB();
 
 const app = express();
 
@@ -16,4 +21,6 @@ app.get('/', (req, res) => {
   res.send('API is running...');
 });
 
-app.listen(port, () => console.log(`Server running on port ${port}`));
+app.listen(port, () =>
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`)
+);
