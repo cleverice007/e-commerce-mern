7f453d686f5a021b0fc67ac247689163beedf1c2
cleverice007 <cleverice90128@gmail.com>
2023-12-22 13:27:35 +0800

    使用dispatch 把add to cart 引入product page

--- frontend/src/pages/ProductPage.tsx
+++ frontend/src/pages/ProductPage.tsx
@@ -4,22 +4,30 @@ import Rating from '../components/Rating';
 import { useState } from 'react';
 import { AiOutlineArrowLeft } from 'react-icons/ai';
 import { useGetProductDetailsQuery } from '../slices/productSlice';
+import { addToCart } from '../slices/cartSlice';
+import { useDispatch } from 'react-redux';
 
 const ProductPage: React.FC = () => {
   const { id: productId } = useParams<{ id: string }>();
-  
-  const {
-    data: product,
-    isLoading,
-    error,
-  } = useGetProductDetailsQuery(productId || 'undefined');
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const [qty, setQty] = useState(1);
+
+  const addToCartHandler = () => {
+    dispatch(addToCart({ ...product!, qty }));
+    navigate('/cart');
+  };
+
+  const { data: product, isLoading, error } = useGetProductDetailsQuery(
+    productId || 'undefined'
+  );
 
   if (isLoading) {
     return <div>Loading...</div>;
   }
 
   if (error) {
-    return <div>Error: {error.toString()}</div>; 
+    return <div>Error: {error.toString()}</div>;
   }
 
   if (!product) {
@@ -28,32 +36,32 @@ const ProductPage: React.FC = () => {
 
   return (
     <>
-      <Link to='/' className='flex items-center text-blue-600 hover:text-blue-800 my-3'>
-        <AiOutlineArrowLeft className='mr-2' /> Go Back
+      <Link to="/" className="flex items-center text-blue-600 hover:text-blue-800 my-3">
+        <AiOutlineArrowLeft className="mr-2" /> Go Back
       </Link>
-      <div className='flex flex-wrap -mx-3'>
-        <div className='w-full md:w-1/2 px-3'>
-          <img className='w-full h-auto object-cover rounded' src={product.image} alt={product.name} />
+      <div className="flex flex-wrap -mx-3">
+        <div className="w-full md:w-1/2 px-3">
+          <img className="w-full h-auto object-cover rounded" src={product.image} alt={product.name} />
         </div>
-        <div className='w-full md:w-1/4 px-3'>
-          <div className='space-y-2'>
-            <h3 className='text-xl font-bold'>{product.name}</h3>
+        <div className="w-full md:w-1/4 px-3">
+          <div className="space-y-2">
+            <h3 className="text-xl font-bold">{product.name}</h3>
             <Rating value={product.rating} text={`${product.numReviews} reviews`} />
             <div>Price: ${product.price}</div>
             <div>{product.description}</div>
           </div>
         </div>
-        <div className='w-full md:w-1/4 px-3'>
-          <div className='border border-gray-200 p-4 rounded'>
-            <div className='mb-2'>
-              <div className='flex justify-between'>
+        <div className="w-full md:w-1/4 px-3">
+          <div className="border border-gray-200 p-4 rounded">
+            <div className="mb-2">
+              <div className="flex justify-between">
                 <div>Price:</div>
-                <div className='font-bold'>${product.price}</div>
+                <div className="font-bold">${product.price}</div>
               </div>
             </div>
 
-            <div className='mb-2'>
-              <div className='flex justify-between'>
+            <div className="mb-2">
+              <div className="flex justify-between">
                 <div>Status:</div>
                 <div>{product.countInStock > 0 ? 'In Stock' : 'Out Of Stock'}</div>
               </div>
@@ -63,8 +71,9 @@ const ProductPage: React.FC = () => {
               className={`w-full bg-blue-500 text-white py-2 rounded ${
                 product.countInStock === 0 ? 'opacity-50 cursor-not-allowed' : ''
               }`}
-              type='button'
+              type="button"
               disabled={product.countInStock === 0}
+              onClick={addToCartHandler} 
             >
               Add To Cart
             </button>
@@ -76,3 +85,4 @@ const ProductPage: React.FC = () => {
 };
 
 export default ProductPage;
+
