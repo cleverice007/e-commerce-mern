63ad4420c25c699f92b4b0efc9ab93e148a07283
cleverice007 <cleverice90128@gmail.com>
2023-12-24 12:05:43 +0800

    import cookie parser in index.js

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
-
+import cookieParser from 'cookie-parser';
 
 import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
@@ -17,6 +17,8 @@ const app = express();
 
 app.use(express.json()); 
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
 
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
