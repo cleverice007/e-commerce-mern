1a27a8c0026484a020a9ef0d9373738641caead9
cleverice007 <cleverice90128@gmail.com>
2023-12-24 17:48:10 +0800

    Create RegisterPage.tsx

---
+++ frontend/src/pages/RegisterPage.tsx
@@ -0,0 +1,138 @@
+import { useState, useEffect } from 'react';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import FormContainer from '../components/FormContainer';
+import { AuthState,UserInfo } from '../slices/authSlice';
+
+import { useRegisterMutation } from '../slices/userApiSlice';
+import { setCredentials } from '../slices/authSlice';
+import { toast } from 'react-toastify';
+
+const RegisterPage: React.FC = () => {
+    const [name, setName] = useState<string>('');
+    const [email, setEmail] = useState<string>('');
+    const [password, setPassword] = useState<string>('');
+    const [confirmPassword, setConfirmPassword] = useState<string>('');
+  
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+  
+    const [register, { isLoading }] = useRegisterMutation();
+  
+    const { userInfo } = useSelector((state: { auth: AuthState }) => state.auth);
+  
+    const { search } = useLocation();
+    const sp = new URLSearchParams(search);
+    const redirect = sp.get('redirect') || '/';
+  
+    useEffect(() => {
+      if (userInfo) {
+        navigate(redirect);
+      }
+    }, [navigate, redirect, userInfo]);
+  
+    const submitHandler = async (e: React.FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+  
+      if (password !== confirmPassword) {
+        toast.error('Passwords do not match');
+        return;
+      }
+  
+      try {
+        const res = await register({ name, email, password }).unwrap();
+        dispatch(setCredentials({ ...res }));
+        navigate(redirect);
+      } catch (error: unknown) {
+        if (error instanceof Error) {
+          toast.error(error.message);
+        } else {
+          toast.error('An unexpected error occurred');
+        }
+      }
+    };
+    return (
+        <div className="container mx-auto p-4">
+          <h1 className="text-xl font-bold">Register</h1>
+          <form onSubmit={submitHandler}>
+            <div className="mb-4">
+              <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="name">
+                Name
+              </label>
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="name"
+                type="text"
+                placeholder="Enter name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
+            </div>
+    
+            <div className="mb-4">
+              <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
+                Email Address
+              </label>
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="email"
+                type="email"
+                placeholder="Enter email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+    
+            <div className="mb-4">
+              <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
+                Password
+              </label>
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="password"
+                type="password"
+                placeholder="Enter password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+    
+            <div className="mb-4">
+              <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="confirmPassword">
+                Confirm Password
+              </label>
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="confirmPassword"
+                type="password"
+                placeholder="Confirm password"
+                value={confirmPassword}
+                onChange={(e) => setConfirmPassword(e.target.value)}
+              />
+            </div>
+    
+            <button
+              className={`bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline ${isLoading && 'opacity-50 cursor-not-allowed'}`}
+              disabled={isLoading}
+              type="submit"
+            >
+              Register
+            </button>
+    
+            {isLoading && <div>Loading...</div>} {/* 替换成你的 Loader 组件 */}
+    
+          </form>
+    
+          <div className="py-3">
+            <span>
+              Already have an account?{' '}
+              <Link to={redirect ? `/login?redirect=${redirect}` : '/login'} className="text-blue-500">
+                Login
+              </Link>
+            </span>
+          </div>
+        </div>
+      );
+    };
+    
+    export default RegisterPage;
