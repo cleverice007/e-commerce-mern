635e928be7d58c2eb64e8cea8a24dba2855e9c6b
cleverice007 <cleverice90128@gmail.com>
2023-12-30 12:48:02 +0800

    在orderpage 新增ordersummary

--- frontend/src/pages/OrderPage.tsx
+++ frontend/src/pages/OrderPage.tsx
@@ -8,7 +8,7 @@ import {
     useGetOrderDetailsQuery,
     usePayOrderMutation,
     useGetPaypalClientIdQuery,
-  } from '../slices/orderApiSlice';import Message from '../components/Message';
+} from '../slices/orderApiSlice'; import Message from '../components/Message';
 
 const OrderPage: React.FC = () => {
     interface OrderItem {
@@ -17,8 +17,8 @@ const OrderPage: React.FC = () => {
         image: string;
         price: number;
         product: string; // product id
-      }
-      
+    }
+
     const { id: orderId } = useParams<{ id: string }>();
 
     const {
@@ -26,77 +26,77 @@ const OrderPage: React.FC = () => {
         refetch,
         isLoading,
         error,
-      } = useGetOrderDetailsQuery(orderId);
-    
+    } = useGetOrderDetailsQuery(orderId);
+
     const [payOrder, { isLoading: loadingPay }] = usePayOrderMutation();
 
     const { userInfo } = useSelector((state: { auth: AuthState }) => state.auth);
 
-  const [{ isPending }, paypalDispatch] = usePayPalScriptReducer();
+    const [{ isPending }, paypalDispatch] = usePayPalScriptReducer();
 
-  const { data: paypal, isLoading: loadingPayPal, error: errorPayPal } = useGetPaypalClientIdQuery(undefined);
+    const { data: paypal, isLoading: loadingPayPal, error: errorPayPal } = useGetPaypalClientIdQuery(undefined);
 
 
-  useEffect(() => {
-    if (!errorPayPal && !loadingPayPal && paypal.clientId) {
-      const loadPaypalScript = async () => {
-        paypalDispatch({
-          type: 'resetOptions',
-          value: {
-            clientId: paypal.clientId, 
-            currency: 'USD',
-          },
-        });
-      };
-      if (order && !order.isPaid) {
-        if (!window.paypal) {
-          loadPaypalScript();
+    useEffect(() => {
+        if (!errorPayPal && !loadingPayPal && paypal.clientId) {
+            const loadPaypalScript = async () => {
+                paypalDispatch({
+                    type: 'resetOptions',
+                    value: {
+                        clientId: paypal.clientId,
+                        currency: 'USD',
+                    },
+                });
+            };
+            if (order && !order.isPaid) {
+                if (!window.paypal) {
+                    loadPaypalScript();
+                }
+            }
         }
-      }
-    }
-  }, [errorPayPal, loadingPayPal, order, paypal, paypalDispatch]);
-  
+    }, [errorPayPal, loadingPayPal, order, paypal, paypalDispatch]);
 
-  function onApprove(data: any, actions: any) {
-    return actions.order.capture().then(async function (details: any) {
-      try {
-        await payOrder({ orderId, details });
-        refetch();
-        toast.success('Order is paid');
-      } catch (err: unknown) {
-        const e = err as { data?: { message?: string }, error?: string }; // 斷言類型
-        toast.error(e.data?.message || e.error || "An unknown error occurred");
-      }
-    });
-  }
-  
-      async function onApproveTest() {
+
+    function onApprove(data: any, actions: any) {
+        return actions.order.capture().then(async function (details: any) {
+            try {
+                await payOrder({ orderId, details });
+                refetch();
+                toast.success('Order is paid');
+            } catch (err: unknown) {
+                const e = err as { data?: { message?: string }, error?: string }; // 斷言類型
+                toast.error(e.data?.message || e.error || "An unknown error occurred");
+            }
+        });
+    }
+
+    async function onApproveTest() {
         await payOrder({ orderId, details: { payer: {} } });
         refetch();
-    
+
         toast.success('Order is paid');
-      }
-    
-      function onError(err: Error) {
+    }
+
+    function onError(err: Error) {
         toast.error(err.message);
-      }
-      
-    
-      function createOrder(data: any, actions: any): Promise<any> {
+    }
+
+
+    function createOrder(data: any, actions: any): Promise<any> {
         return actions.order
-          .create({
-            purchase_units: [
-              {
-                amount: { value: order.totalPrice.toString() },
-              },
-            ],
-          })
-          .then((orderID: any) => {
-            return orderID;
-          });
-      }
-    
-    
+            .create({
+                purchase_units: [
+                    {
+                        amount: { value: order.totalPrice.toString() },
+                    },
+                ],
+            })
+            .then((orderID: any) => {
+                return orderID;
+            });
+    }
+
+
     return (
         <div className="container mx-auto mt-10">
             <h1 className="text-3xl font-semibold mb-4">Order {order?._id}</h1>
@@ -106,7 +106,8 @@ const OrderPage: React.FC = () => {
                         {isLoading ? (
                             <div>Loading...</div>
                         ) : (
-                            <div>Error: {error.toString()}</div>)}
+                            <div>Error: {error.toString()}</div>
+                        )}
                     </div>
                 ) : (
                     <>
@@ -152,7 +153,7 @@ const OrderPage: React.FC = () => {
                                 <Message>Order is empty</Message>
                             ) : (
                                 <div>
-                                    {order?.orderItems.map((item:OrderItem, index:number) => (
+                                    {order?.orderItems.map((item: OrderItem, index: number) => (
                                         <div key={index} className="mb-4">
                                             {/* Item details */}
                                             <div className="flex items-center">
@@ -179,6 +180,36 @@ const OrderPage: React.FC = () => {
                                     ))}
                                 </div>
                             )}
+                        </div>
+                        {/* Order Summary */}
+                        <div className="col-span-2 md:col-span-1">
+                            <div className="bg-white shadow-md rounded my-6 p-6">
+                                <h2 className="text-xl font-semibold mb-3">Order Summary</h2>
+                                <div className="mb-4">
+                                    <div className="flex justify-between">
+                                        <div>Items</div>
+                                        <div>${order.itemsPrice}</div>
+                                    </div>
+                                </div>
+                                <div className="mb-4">
+                                    <div className="flex justify-between">
+                                        <div>Shipping</div>
+                                        <div>${order.shippingPrice}</div>
+                                    </div>
+                                </div>
+                                <div className="mb-4">
+                                    <div className="flex justify-between">
+                                        <div>Tax</div>
+                                        <div>${order.taxPrice}</div>
+                                    </div>
+                                </div>
+                                <div className="mb-4">
+                                    <div className="flex justify-between">
+                                        <div>Total</div>
+                                        <div>${order.totalPrice}</div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </>
                 )}
