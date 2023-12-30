a289eb48561d37438fb34fdeac5e1138d781a56f
cleverice007 <cleverice90128@gmail.com>
2023-12-30 16:50:38 +0800

    create new orderRoutes ( import updateOrderToPaid function

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -11,6 +11,7 @@ import { protect, admin } from '../middleware/authMiddleware.js';
 router.route('/').post(protect, addOrderItems);
 router.route('/mine').get(protect, getMyOrders);
 router.route('/:id').get(protect, getOrderById);
+router.route('/:id/pay').put(protect, updateOrderToPaid);
 
 
 export default router;
