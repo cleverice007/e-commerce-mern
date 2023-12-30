0a83fa77f43d2d9fae1b292e5d21883caf01c094
cleverice007 <cleverice90128@gmail.com>
2023-12-30 12:08:48 +0800

    引入react paypal sdk 的function
    1. paypaldispatch
    2. onApprove
    3. onError
    4. createOrder

--- frontend/src/pages/OrderPage.tsx
+++ frontend/src/pages/OrderPage.tsx
@@ -1,6 +1,14 @@
 import { useParams, Link } from 'react-router-dom';
-import { useGetOrderDetailsQuery } from '../slices/orderApiSlice';
-import Message from '../components/Message';
+import { useEffect } from 'react';
+import { PayPalButtons, usePayPalScriptReducer } from '@paypal/react-paypal-js';
+import { useSelector } from 'react-redux';
+import { toast } from 'react-toastify';
+import { AuthState } from '../slices/authSlice';
+import {
+    useGetOrderDetailsQuery,
+    usePayOrderMutation,
+    useGetPaypalClientIdQuery,
+  } from '../slices/orderApiSlice';import Message from '../components/Message';
 
 const OrderPage: React.FC = () => {
     interface OrderItem {
@@ -13,8 +21,82 @@ const OrderPage: React.FC = () => {
       
     const { id: orderId } = useParams<{ id: string }>();
 
-    const { data: order, isLoading, error } = useGetOrderDetailsQuery(orderId);
+    const {
+        data: order,
+        refetch,
+        isLoading,
+        error,
+      } = useGetOrderDetailsQuery(orderId);
+    
+    const [payOrder, { isLoading: loadingPay }] = usePayOrderMutation();
 
+    const { userInfo } = useSelector((state: { auth: AuthState }) => state.auth);
+
+  const [{ isPending }, paypalDispatch] = usePayPalScriptReducer();
+
+  const { data: paypal, isLoading: loadingPayPal, error: errorPayPal } = useGetPaypalClientIdQuery(undefined);
+
+
+  useEffect(() => {
+    if (!errorPayPal && !loadingPayPal && paypal.clientId) {
+      const loadPaypalScript = async () => {
+        paypalDispatch({
+          type: 'resetOptions',
+          value: {
+            clientId: paypal.clientId, 
+            currency: 'USD',
+          },
+        });
+      };
+      if (order && !order.isPaid) {
+        if (!window.paypal) {
+          loadPaypalScript();
+        }
+      }
+    }
+  }, [errorPayPal, loadingPayPal, order, paypal, paypalDispatch]);
+  
+
+  function onApprove(data: any, actions: any) {
+    return actions.order.capture().then(async function (details: any) {
+      try {
+        await payOrder({ orderId, details });
+        refetch();
+        toast.success('Order is paid');
+      } catch (err: unknown) {
+        const e = err as { data?: { message?: string }, error?: string }; // 斷言類型
+        toast.error(e.data?.message || e.error || "An unknown error occurred");
+      }
+    });
+  }
+  
+      async function onApproveTest() {
+        await payOrder({ orderId, details: { payer: {} } });
+        refetch();
+    
+        toast.success('Order is paid');
+      }
+    
+      function onError(err: Error) {
+        toast.error(err.message);
+      }
+      
+    
+      function createOrder(data: any, actions: any): Promise<any> {
+        return actions.order
+          .create({
+            purchase_units: [
+              {
+                amount: { value: order.totalPrice.toString() },
+              },
+            ],
+          })
+          .then((orderID: any) => {
+            return orderID;
+          });
+      }
+    
+    
     return (
         <div className="container mx-auto mt-10">
             <h1 className="text-3xl font-semibold mb-4">Order {order?._id}</h1>
