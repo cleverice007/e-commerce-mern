4d6d97ad9e89c0ece750afc5071e5b66c8a847e5
cleverice007 <cleverice90128@gmail.com>
2024-01-02 13:18:21 +0800

    updateOrderToDelivered function

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -90,6 +90,7 @@ const getMyOrders = asyncHandler(async (req, res) => {
       throw new Error('Order not found');
     }
   });
+
 // @desc    Get all orders
 // @route   GET /api/orders
 // @access  Private/Admin
@@ -98,10 +99,31 @@ const getOrders = asyncHandler(async (req, res) => {
   res.json(orders);
 });
 
+// @desc    Update order to delivered
+// @route   PUT /api/orders/:id/deliver
+// @access  Private/Admin
+
+const updateOrderToDelivered = asyncHandler(async (req, res) => {
+  const order = await Order.findById(req.params.id);
+
+  if (order) {
+    order.isDelivered = true;
+    order.deliveredAt = Date.now();
+
+    const updatedOrder = await order.save();
+
+    res.json(updatedOrder);
+  } else {
+    res.status(404);
+    throw new Error('Order not found');
+  }
+});
+
 export {
   addOrderItems,
   getOrderById,
   getMyOrders,
   updateOrderToPaid,
   getOrders,
+  updateOrderToDelivered
 };
