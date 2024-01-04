edfb242d770fdb60e87f950ff5a364660f8fa77e
cleverice007 <cleverice90128@gmail.com>
2024-01-04 14:52:48 +0800

    createProductReview function

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -2,10 +2,11 @@
 import express from 'express';
 const router = express.Router();
 
-import { getProducts, getProductById,deleteProduct} from '../controllers/productController.js';
+import { getProducts, getProductById,deleteProduct,createProductReview} from '../controllers/productController.js';
 import { protect, admin } from '../middleware/authMiddleware.js';
 
 router.route('/').get(getProducts);
+router.route('/:id/reviews').post(protect, createProductReview);
 router.route('/:id').get(getProductById).delete(protect, admin, deleteProduct);
 ;
 
