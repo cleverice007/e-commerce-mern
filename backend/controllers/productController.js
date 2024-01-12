be2e0e22fe2de9b33ed74870f866e0e9b8d33cff
cleverice007 <cleverice90128@gmail.com>
2024-01-12 11:31:48 +0800

    deleteProduct 新增從redis刪除

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -47,16 +47,23 @@ const getProducts = asyncHandler(async (req, res) => {
 // @route   DELETE /api/products/:id
 // @access  Private/Admin
 const deleteProduct = asyncHandler(async (req, res) => {
-  const product = await Product.findById(req.params.id);
+  const productId = req.params.id;
+  const product = await Product.findById(productId);
 
   if (product) {
+    // 從 MongoDB 刪除產品
     await Product.deleteOne({ _id: product._id });
+
+    // 從 Redis 刪除產品
+    await redisClient.del(`product:${productId}`);
+
     res.json({ message: 'Product removed' });
   } else {
     res.status(404);
     throw new Error('Product not found');
   }
 });
+
 
 
 // @desc    Create new review
--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,7 +2,6 @@ import asyncHandler from '../middleware/asyncHandler.js';
 import generateToken from '../utils/generateToken.js';
 import User from '../models/userModel.js';
 import redisClient from '../config/redis.js';
-import createSession from '../utils/session.js';
 
 // @desc    Auth user & get token
 // @route   POST /api/users/auth
